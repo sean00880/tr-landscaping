21826fe9f6abae9bf7829226aedb5ae72ad67dbf
sean00880 <theelitereapersanctum@gmail.com>
2023-02-17 21:36:10 -0500

    commit7

--- layout/title.js
+++ layout/title.js
@@ -4,7 +4,7 @@ import Head from 'next/head'
 export default function Title({title}) {
     return (
         <Head>
-            <title>Industify | {title}</title>
+            <title>TR Landsacping LLC | {title}</title>
         </Head>
     )
 }
