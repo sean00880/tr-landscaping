1e7b07efc11b72c7d5fe46b8a0f4bcdbada1eb54
sean00880 <theelitereapersanctum@gmail.com>
2023-02-17 21:11:43 -0500

    commit4

--- next.config.js
+++ next.config.js
@@ -2,5 +2,10 @@
 
 const nextConfig = {
     optimizeFonts: false,
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
  }
  module.exports = nextConfig
